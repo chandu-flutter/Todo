b1e133f2ef4e38d3b0e107b31747ddbda7b9c945
chandu <chandu.christ@gmail.com>
2023-10-21 16:36:01 +0530

    edited home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -51,7 +51,7 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                       ),
-                      for (ToDo todoo in _foundToDo)
+                      for (ToDo todoo in _foundToDo.reversed)
                         ToDoItem(
                           todo: todoo,
                           onToDoChanged: _handleToDoChange,
